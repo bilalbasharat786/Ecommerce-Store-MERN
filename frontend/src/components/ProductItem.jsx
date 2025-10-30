fa16ef77357018d1dcdaa25c8a0b1f59baeb1e17
Bilal Basharat <hassanrohan366@gmail.com>
2025-10-30 20:49:28 +0500

    discount price

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 
 const ProductItem = ({ id, image, name, price, discountPrice }) => {
   const { currency } = useContext(ShopContext);
+console.log("💸 Product Discount Price:", discountPrice);
 
+const finalPrice =
+    discountPrice && discountPrice < price ? discountPrice : price;
   return (
     <Link
       className="text-gray-700 cursor-pointer group w-full max-w-xs sm:max-w-sm mx-auto block"
@@ -32,10 +35,24 @@ const ProductItem = ({ id, image, name, price, discountPrice }) => {
       {/* Product Info */}
       <div className="pt-3 pb-1 text-center sm:text-left">
         <p className="text-sm sm:text-lg font-semibold truncate px-2">{name}</p>
-        <p className="text-xs sm:text-sm font-medium text-gray-600 px-2">
-          {currency} {price}
-        </p>
+        <div className="flex justify-center sm:justify-start gap-2 px-2 items-center">
+          {discountPrice && discountPrice < price ? (
+            <>
+              <p className="text-xs sm:text-sm font-medium text-gray-400 line-through">
+                {currency}{Number(price).toLocaleString()}
+              </p>
+              <p className="text-xs sm:text-sm font-semibold text-red-500">
+                {currency}{Number(discountPrice).toLocaleString()}
+              </p>
+            </>
+          ) : (
+            <p className="text-xs sm:text-sm font-medium text-gray-600">
+              {currency}{Number(price).toLocaleString()}
+            </p>
+          )}
+        </div>
       </div>
+
     </Link>
   );
 };
