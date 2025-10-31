9a2ecfea32be91ca8aae7d1a0b7cd12c67324d7c
Bilal Basharat <hassanrohan366@gmail.com>
2025-10-31 06:58:09 +0500

    extra zero removed

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -74,22 +74,22 @@ const Product = () => {
           </div>
 
           {/* Price */}
-         <p className="mt-4 text-2xl sm:text-3xl font-medium text-gray-900">
+   <p className="mt-4 text-2xl sm:text-3xl font-medium text-gray-900">
   {currency}
   {Number(
     productData.discountPrice && productData.discountPrice < productData.price
       ? productData.discountPrice
       : productData.price
   ).toLocaleString()}
-
   {productData.discountPrice &&
     productData.discountPrice < productData.price && (
       <span className="line-through text-gray-500 text-lg sm:text-xl ml-4">
         {currency}
         {Number(productData.price).toLocaleString()}
       </span>
-  )}
+    )}
 </p>
+
 
 
 
