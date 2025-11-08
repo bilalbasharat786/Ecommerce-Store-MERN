80c59945c6ba8abf4d92cf63cde0d0edc5fc1f60
Bilal Basharat <hassanrohan366@gmail.com>
2025-11-08 16:37:10 +0500

    multer

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,31 +1,11 @@
 import multer from "multer";
-import path from "path";
 
-// Upload storage config
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/"); // uploads folder (root me hona chahiye)
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      `${Date.now()}-${file.fieldname}${path.extname(file.originalname)}`
-    );
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
   },
 });
 
-// File filter (optional safety)
-const fileFilter = (req, file, cb) => {
-  const allowed = /jpeg|jpg|png|webp/;
-  const ext = path.extname(file.originalname).toLowerCase();
-  if (allowed.test(ext)) cb(null, true);
-  else cb(new Error("Only image files are allowed"));
-};
-
-const upload = multer({
-  storage,
-  fileFilter,
-});
+const upload = multer({ storage });
 
 export default upload;
-
