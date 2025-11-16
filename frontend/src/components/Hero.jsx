b6b504d472608418353e477415d0c85469ffd8e9
Bilal Basharat <hassanrohan366@gmail.com>
2025-11-16 18:30:50 +0500

    app.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import axios from "axios";
-import { backendUrl } from "../App"; 
+import { backendUrl } from "../App.jsx"; 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
