7b76adc155d97f0313f4bbfdf3951eef447f08bf
Bilal Basharat <hassanrohan366@gmail.com>
2025-11-27 09:54:12 +0500

    wish9

--- frontend/src/contexts/WishlistContext.jsx
+++ frontend/src/contexts/WishlistContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState ,useContext} from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { ShopContext } from "./ShopContext";
