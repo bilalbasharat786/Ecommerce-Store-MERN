5f8fbf6c3501468fae2eba9d3314b5d40ec263fc
Bilal Basharat <hassanrohan366@gmail.com>
2025-11-30 08:59:36 +0500

    refactor: remove WishlistContextProvider import from main application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./contexts/ShopContext.jsx";
-import WishlistContextProvider from "./contexts/WishlistContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
