2d55e35f780445ca5380af14908728d66569148d
sfmtechnicals-art <sfmtechnicals@gmail.com>
2026-02-11 11:15:56 +0500

    middleware hata diya userRoutes.js sa

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,5 @@
 import express from "express";
 import { loginUser, registerUser, adminLogin, googleLogin ,addToWishlist, getUserWishlist} from "../controllers/userController.js";
-import { authUser } from "../middlewares/auth.js";
-
-
 // ⭐ Auth middleware (name aapka hoga authUser ya verifyUser)
 
 const userRouter = express.Router();
@@ -10,8 +7,8 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/admin", adminLogin);
-userRouter.post('/add-to-wishlist', authUser, addToWishlist);
-userRouter.post('/get-wishlist', authUser, getUserWishlist);
+userRouter.post('/add-to-wishlist',  addToWishlist);
+userRouter.post('/get-wishlist', getUserWishlist);
 // ⭐ NEW: Google Login Route
 userRouter.post("/google-login", googleLogin);
 
