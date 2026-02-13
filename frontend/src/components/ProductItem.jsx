da6f1b409f4a4964bf7837019a8a5bd76f32b0ec
sfmtechnicals-art <sfmtechnicals@gmail.com>
2026-02-13 20:19:53 +0500

    color option home page sa hata diya

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -85,18 +85,7 @@ const finalPrice =
   <span>{`PKR ${price}`}</span>
 )}
 
-{/* ⭐ Color Options */}
-{colors && colors.length > 0 && (
-  <div className="flex justify-center sm:justify-start gap-2 px-2 mt-2">
-    {colors.map((clr, index) => (
-      <span
-        key={index}
-        style={{ backgroundColor: clr }}
-        className="w-4 h-4 rounded-full border border-gray-300"
-      ></span>
-    ))}
-  </div>
-)}
+
 
         </div>
       </div>
